c235fa9a2d96ee3dcc7bf0a76cde5f4546805e91
Sudhanshu Garg <sudhanshugarg85@gmail.com>
2013-06-24 12:27:36 -0700

    moved old files into new directories

--- constants.js
+++
@@ -1,9 +0,0 @@
-var Constants = {
-    HONEYCOMB: 0,
-    SQUARE: 1,
-    SliceViewWidth: 500,
-    SliceViewHeight: 500,
-    Renderer: 'two',
-    RendererTwo: 'two',
-    RendererThree: 'three'
-};
